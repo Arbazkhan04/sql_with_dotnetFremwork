0da0ae2dbc3404c30f713b6f4131cded46de5791
Arbaz khan <arbaz582021@gmail.com>
2024-01-27 12:38:52 +0500

    add

---
+++ basicCrud/basicCrud/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCrud
+{
+     class Configuration
+     {
+        //Server=myServerAddress;Database=myDataBase;Integrated Security = True
+        String ConnectionStr = @"Data Source=(local);Initial Catalog=tes_db;Integrated Security=True";
+        SqlConnection con;
+        private static Configuration _instance;
+        public static Configuration getInstance()
+        {
+            if (_instance == null)
+                _instance = new Configuration();
+            return _instance;
+        }
+        private Configuration()
+        {
+            con = new SqlConnection(ConnectionStr);
+            con.Open();
+        }
+        public SqlConnection getConnection()
+        {
+            return con;
+        }
+     }
+}
---
+++ basicCrud/basicCrud/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace basicCrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Insert into employee values (@a, @num)", con);
+            cmd.Parameters.AddWithValue("@a", textBox1.Text);
+            cmd.Parameters.AddWithValue("@num", textBox2.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully saved");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from employee", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
